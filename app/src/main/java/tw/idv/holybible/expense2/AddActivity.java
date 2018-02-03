a3414f39f3792f13e55e93510e18395bb459d05a
CChang <holybible934@gmail.com>
2018-02-03 23:41:12 +0800

    Functions of AddActivity and ExpenseHelper from SQLLiteOpenHelper

--- app/src/main/java/tw/idv/holybible/expense2/AddActivity.java
+++ app/src/main/java/tw/idv/holybible/expense2/AddActivity.java
@@ -2,12 +2,34 @@ package tw.idv.holybible.expense2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
+
+
+    private final String TAG = AddActivity.class.getSimpleName();
+    private EditText expName;
+    private EditText edAmount;
+    private EditText expDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        findViews();
+    }
+
+    private void findViews() {
+        expName = findViewById(R.id.edtxtExpName);
+        edAmount = findViewById(R.id.edtxtAmount);
+        expDate = findViewById(R.id.edtxtDate);
+    }
+
+    public void add() {
+        String name = expName.getText().toString();
+        String date = expDate.getText().toString();
+        int amount = Integer.parseInt(edAmount.getText().toString());
+        Log.d(TAG, "add: " + amount);
     }
 }
---
+++ app/src/main/java/tw/idv/holybible/expense2/ExpenseHelper.java
@@ -0,0 +1,27 @@
+package tw.idv.holybible.expense2;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by chchang on 2018/2/3.
+ */
+
+public class ExpenseHelper extends SQLiteOpenHelper {
+
+    public ExpenseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
