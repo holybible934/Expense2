1739467c0877495dcef7546b43544d821e93faae
CChang <holybible934@gmail.com>
2018-02-12 01:01:16 +0800

    Insert record in AddActivity

--- app/src/main/java/tw/idv/holybible/expense2/AddActivity.java
+++ app/src/main/java/tw/idv/holybible/expense2/AddActivity.java
@@ -1,7 +1,9 @@
 package tw.idv.holybible.expense2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentValues;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -32,5 +34,24 @@ public class AddActivity extends AppCompatActivity {
         String date = expDate.getText().toString();
         int amount = Integer.parseInt(edAmount.getText().toString());
         Log.d(TAG, "add: " + amount);
+        ExpenseHelper helper = new ExpenseHelper(this);
+        ContentValues values = new ContentValues();
+        values.put("info", name);
+        values.put("cdate", date);
+        values.put("amount", amount);
+        long cursorId = helper.getWritableDatabase()
+                .insert(ExpenseContract.EXPENSE_TABLE, null, values);
+        if (cursorId == -1) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Database Error!")
+                    .setMessage("Insert Failed")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+        else {
+            Log.d(TAG, "add: Insert succeeded!");
+            helper.close();
+            finish();
+        }
     }
 }
